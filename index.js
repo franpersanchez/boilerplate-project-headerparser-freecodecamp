39e61f0bc3e77903781bb428e1043c2a7c5fa1ee
Fran Pérez <78259026+franpersanchez@users.noreply.github.com>
2022-07-09 22:08:50 +0000

    finalised

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
+//require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -23,6 +23,23 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+
+app.get("/api/whoami", function(req, res){
+const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const lang = req.headers["accept-language"];
+  const softw = req.headers["user-agent"];
+  
+  res.json({
+      ipaddress: ip,
+      language:lang,
+      software:softw,  
+      
+    
+  });
+  
+  
 });
 
 
